c644f783ebdf24a324e4f61803ffbcc8a2fd894d
Ubuntu <ubuntu@ip-172-31-28-83.ec2.internal>
2019-09-16 17:26:35 +0000

    finished unit 2-01 python

---
+++ radius_calculations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by DJ Watson
+# Created on September 2019
+# This is a program for finidng the area and perimeter of a circles
+
+
+import math
+
+
+def main():
+    print("If a circle has a radius of 15mm")
+    print("AREA: {}mm^2".format(math.pi*15**2))
+    print("PERIMETER: {}mm".format(2*math.pi*15))
+
+
+if __name__ == "__main__":
+    main()
